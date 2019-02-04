cf1a7385bbc85f2b0eeae7c2258d877e94c98c8f
Remi Gavin <remi.gavin38@gmail.com>
2019-02-04 20:30:56 +0100

    Update to webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,58 @@
 const path = require('path')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const dev = process.env.NODE_ENV === "dev"
-
-let cssLoaders = [
-    dev ? 'style-loader' : MiniCssExtractPlugin.loader,
-    { loader: 'css-loader', options: { importLoaders: 1 } },
-]
-if(!dev)
-{
-    cssLoaders.push({
-        loader: 'postcss-loader',
-        options: {
-            plugins: (loader) => [
-                require('autoprefixer')({
-                    browsers: ['last 2 versions', 'ie' > 8]
-                })
-            ],
-            minimize: !dev
-        }
-    })
-}
-
-module.exports = {
-    w
-    watch: dev,
-    devtool: dev ? "cheap-module-eval-source-map" : false,
-    output: {
-        path: path.resolve('./dist'), // Path of result files
-        filename: '[name].js',
-        publicPath: "dist"
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
+module.exports = (env, argv) => {
+    let dev = argv.mode === "development"
+    let cssLoaders = [
+        dev ? 'style-loader' : MiniCssExtractPlugin.loader,
+        { loader: 'css-loader', options: { importLoaders: 1 } },
+    ]
+    if(!dev)
+    {
+        cssLoaders.push({
+            loader: 'postcss-loader',
+            options: {
+                plugins: (loader) => [
+                    require('autoprefixer')({
+                        browsers: ['last 2 versions', 'ie' > 8]
+                    })
+                ],
+            }
         })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader', // Use last ES syntax
-                }
-            },
-            {
-                test: /\.css$/,
-                use: cssLoaders,
-            },
-            {
-                test: /\.scss$/,
-                use: [...cssLoaders, 'sass-loader'],
-            },
-        ]
+    }
+    return {
+        entry: {
+          app: './src/index.js'
+        },
+        watch: dev,
+        devtool: dev ? "cheap-module-eval-source-map" : false,
+        output: {
+            path: path.resolve('./dist'), // Path of result files
+            filename: '[name].js',
+            publicPath: "dist"
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader', // Use last ES syntax
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: cssLoaders,
+                },
+                {
+                    test: /\.scss$/,
+                    use: [...cssLoaders, 'sass-loader'],
+                },
+            ]
+        }
     }
 }
